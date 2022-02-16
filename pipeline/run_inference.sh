56fb6e4651a5dfe1ac3f35feecc51e4fd7b3193a
Ramya Muthukrishnan <51800150+ramyamut@users.noreply.github.com>
2022-02-16 17:36:16 -0500

    update inference script

--- pipeline/run_inference.sh
+++ pipeline/run_inference.sh
@@ -14,7 +14,16 @@ breast_masks="${output_dir}/breast_masks_inference"
 mkdir ${breast_masks}
 python3 scripts/inference.py "${preproc_dir}/breast_net_data/image/" ${breast_masks} ${breast_model}
 
+# produce density model inputs
+dense_preproc_dir="${output_dir}/dense_inputs"
+mkdir ${dense_preproc_dir}
+python3 scripts/preprocess_density_inputs.py "${preproc_dir}/breast_net_data/image/" ${breast_masks} ${dense_preproc_dir}
+
 # run dense tissue segmentation model
 dense_masks="${output_dir}/dense_masks_inference"
 mkdir ${dense_masks}
 python3 scripts/inference.py ${dense_preproc_dir} ${dense_masks} ${dense_model}
+
+# postprocess masks
+
+# PD calculation
