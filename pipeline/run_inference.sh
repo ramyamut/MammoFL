deb8eb0dd3c31c02360bdc8503e2ac1884005d02
Ramya Muthukrishnan <51800150+ramyamut@users.noreply.github.com>
2022-02-16 16:59:00 -0500

    update inference script

--- pipeline/run_inference.sh
+++ pipeline/run_inference.sh
@@ -3,3 +3,18 @@ input_dicoms=${1}
 breast_model=${2}
 dense_model=${3}
 output_dir=${4}
+
+# preprocess images
+preproc_dir="${output_dir}/preproc_imgs"
+mkdir ${preproc_dir}
+python3 scripts/execute_libra_preprocessing.py -i ${input_dicoms} -o ${preproc_dir}
+
+# run breast segmentation model
+breast_masks="${output_dir}/breast_masks_inference"
+mkdir ${breast_masks}
+python3 scripts/inference.py "${preproc_dir}/breast_net_data/image/" ${breast_masks} ${breast_model}
+
+# run dense tissue segmentation model
+dense_masks="${output_dir}/dense_masks_inference"
+mkdir ${dense_masks}
+python3 scripts/inference.py ${dense_preproc_dir} ${dense_masks} ${dense_model}
