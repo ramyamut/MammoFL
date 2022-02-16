21ed1cab19234356d6c94c6d7506c0b84a11291a
Ramya Muthukrishnan <51800150+ramyamut@users.noreply.github.com>
2022-02-15 23:31:18 -0500

    add more utilities

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+import cv2
 import torch
 
 def mse_loss(output, target):
@@ -24,3 +27,57 @@ def dice_loss(output, target):
     score = (2.0 * intersection + smooth) / (torch.sum(iflat, dim=1) + torch.sum(tflat, dim=1) + smooth)
     loss = 1 - score
     return torch.mean(loss)
+
+def normalize_image(IMG):
+    min = IMG.min()
+    max = IMG.max()
+    out = (IMG - min) / (max - min)
+    out = out * 255
+    try:
+        out[out>255] = 255
+    except:
+        out = out
+    try:
+        out[out<0] = 0
+    except:
+        out = out
+    out = out.astype('uint8')
+    return out
+
+def fix_ratio(IMG, height, width, method="area"):
+    Flag = 0
+    if np.array_equal(IMG, IMG.astype(bool)):
+        IMG = IMG.astype("uint8")
+        Flag = 1
+
+    MIN = IMG.min()
+
+    if IMG.shape[0] > IMG.shape[1]:
+        IMG = np.concatenate((IMG, np.ones([IMG.shape[0],
+                            IMG.shape[0]-IMG.shape[1]])*MIN), axis=1)
+    else:
+        IMG = np.concatenate((IMG, np.ones([IMG.shape[1]-IMG.shape[0],
+                            IMG.shape[1]])*MIN), axis=0)
+
+    if method=="area":
+        IMG = cv2.resize(IMG, (height, width), interpolation=cv2.INTER_AREA)
+    if method=="linear":
+        IMG = cv2.resize(IMG, (height, width), interpolation=cv2.INTER_LINEAR)
+    if method=="cubic":
+        IMG = cv2.resize(IMG, (height, width), interpolation=cv2.INTER_CUBIC)
+    if method=="nearest":
+        IMG = cv2.resize(IMG, (height, width), interpolation=cv2.INTER_NEAREST)
+    elif method=="lanc":
+        IMG = cv2.resize(IMG, (height, width), interpolation=cv2.INTER_LANCZOS4)
+
+    return(IMG)
+
+def object_oriented_preprocessing(MASK, sub, csv_path):
+    df = pd.read_csv(csv_path)
+    if 'FieldOfViewHorizontalFlip' in df.columns and df['FieldOfViewHorizontalFlip'][0]=='YES':
+        if df['ImageLaterality'][0] == 'L':
+            MASK=np.fliplr(MASK)
+    else:
+        if df['ImageLaterality'][0] == 'R':
+            MASK=np.fliplr(MASK)
+    return MASK
