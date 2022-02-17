83b5634dfcaff93e72fc3ad59b05dbdc036adc2f
Ramya Muthukrishnan <51800150+ramyamut@users.noreply.github.com>
2022-02-16 19:45:52 -0500

    add inference script

---
+++ scripts/inference.py
@@ -0,0 +1,34 @@
+import torch
+import glob
+import sys
+import imageio
+import numpy as np
+from torchvision import transforms
+from models import FederatedUNet
+
+IMAGE_DIR = sys.argv[1]
+MASK_DIR = sys.argv[2]
+MODEL_PATH = sys.argv[3]
+
+torch.manual_seed(42)
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = FederatedUNet().to(device)
+model.eval().load_state_dict(torch.load(MODEL_PATH, map_location='cpu'))
+image_paths = glob.glob(f'{IMAGE_DIR}/*.png')
+img_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+for path in image_paths:
+    sub_id = path.split('/')[-1]
+    sub_id = path.split('.png')[0]
+    img = imageio.imread(path) / 255
+    if img.ndim == 2:
+        img = np.expand_dims(img, axis=2)
+        img = np.concatenate([img]*3, axis=2)
+    img_transformed = img_transform(img)
+    img_transformed = img_transformed.unsqueeze(0).to(device)
+    pred_mask = model(img_transformed.float()).squeeze()
+    pred_mask = torch.round(pred_mask)
+    pred_mask = pred_mask.cpu().detach().numpy()
+    pred_mask = (pred_mask*255).astype('uint8')
+    imageio.imwrite(f'{MASK_DIR}/{sub_id}.png', pred_mask)
